78e1d60b1f2d702b81ab8712d5a3ff17d6f33ac2
LuckyWolf-svg <134198480+LuckyWolf-svg@users.noreply.github.com>
2024-08-10 21:22:47 +0700

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -2,8 +2,6 @@ from gpt4all import GPT4All
 model = GPT4All(model_name="Meta-Llama-3-8B-Instruct.Q4_0.gguf", device='gpu')
 #You can change this model to another, not official gpt4all
 
-character_description = "Ты пират"
-
 print('Test 1/2 | Success')
 
 def gpt (text):
